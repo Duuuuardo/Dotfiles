b54fa59015ecf302c3b5f38a4df9d4a1db1588d9
duuuuardo <eduardofabisiak@proton.me>
2023-12-09 18:51:58 -0300

    mpv configuration

--- modules/apps/mpv/default.nix
+++ modules/apps/mpv/default.nix
@@ -5,50 +5,40 @@
      mpv 
     ];
 
-    home.sessionVariables = {
-      MOZ_ENABLE_WAYLAND = 1;
-    };
-    
     programs = {
-      chromium = {
-    	enable = true;
+      mpv = {
+        enable = true;
+        bindings = {
+          l = "seek  5";
+          h = "seek -5";
+          k = "seek  30";
+          j = "seek -30";
+          J = "cycle sub";
+          K = "cycle sub down";
+        };
+        config = {
+          force-window = true;
+          osc = true;
+          hwdec = "auto-safe";
+          vo = "gpu";
+          profile = "gpu-hq";
+          gpu-context = "wayland";
+          sub-border-size = 4.0;
+          sub-shadow-color = "0.0/0.0/0.0";
+          sub-shadow-offset = 1;
+          sub-auto = "fuzzy";
+          msg-level = "all=error";
+          # ytdl-format = "bestvideo[height<=?1080]+bestaudio/best";
+          ytdl-format = "best";
+          save-position-on-quit = true;
+          slang = "eng,en,Eng,English";
+          alang = "br,en,eng";
+          sub-font = "Impress BT";
+          autofit = "50%";
+          sub-font-size = "48";
+          speed = "1.5";
+        };
       };
-      firefox.enable = true;
     };
   };
-
-mpv = {
-  enable = true;
-  bindings = {
-    l = "seek  5";
-    h = "seek -5";
-    k = "seek  30";
-    j = "seek -30";
-    J = "cycle sub";
-    K = "cycle sub down";
-  };
-
-  config = {
-    force-window = true;
-    osc = true;
-    hwdec = "auto-safe";
-    vo = "gpu";
-    profile = "gpu-hq";
-    gpu-context = "wayland";
-    sub-border-size = 4.0;
-    sub-shadow-color = "0.0/0.0/0.0";
-    sub-shadow-offset = 1;
-    sub-auto = "fuzzy";
-    msg-level = "all=error";
-    # ytdl-format = "bestvideo[height<=?1080]+bestaudio/best";
-    ytdl-format = "best";
-    save-position-on-quit = true;
-    slang = "eng,en,Eng,English";
-    alang = "br,en,eng";
-    sub-font = "Impress BT";
-    autofit = "50%";
-    sub-font-size = "48";
-    speed = "1.5";
-  };
-};
 }
